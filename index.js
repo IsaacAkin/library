8e2cd382e556187ada313069229b05bb983b3934
Isaac Akinsanya <IsaacAki247@gmail.com>
2025-04-30 22:49:20 +0100

    created a function to display the books on the page
    - created a container variable to allow for the appending of books onto the html page
    
    - added dummy test book data to the array to visualise what is happening
    
    - created the function 'displayBooks'. The function uses a for loop to go through each book in the myLibrary array, creating a div and displaying the information of each book inside of it and then appending the div of book details onto the already created container.

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const myLibrary = [];
 
+const container = document.querySelector('.container');
+
 function Book(title, author, pages, hadRead) {
     if (!new.target) {
         throw Error("You can only call this object with the 'new' operator");
@@ -16,3 +18,27 @@ function addBookToLibrary(title, author, pages, hasRead) {
     const newBook = new Book(title, author, pages, hasRead);
     myLibrary.push(newBook);
 }
+
+addBookToLibrary('Mushoku Tensei', 'Rifujin na Magonete', 20, true);
+addBookToLibrary('Domestic na Kanojo', 'Kei Sasuga', 12, true);
+addBookToLibrary('One Piece', 'Eiichiro Oda', 100, true);
+
+function displayBooks(library) {
+    library.forEach(book => {
+        let div = document.createElement('div');
+
+        let title = book.title;
+        let author = book.author;
+        let pages = book.pages;
+        let hasRead = book.hasRead;
+
+        div.append(title);
+        div.append(author);
+        div.append(pages);
+        div.append(hasRead);
+
+        container.appendChild(div);
+    });
+}
+
+displayBooks(myLibrary);
